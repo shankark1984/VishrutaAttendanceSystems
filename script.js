0eef7e2f7205c8874db22e40ae55f91511b54978
shankark1984 <46882965+shankark1984@users.noreply.github.com>
2024-07-05 16:38:17 +0530

    Update script.js
    Employee info

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ domReady(function () {
 	// If found you qr code
 	function onScanSuccess(decodeText, decodeResult) {
 
-		alert("You Qr is : " + decodeText, decodeResult);
+		alert("Employee Info : " + decodeText, decodeResult);
 		alert("Attendance Successfully")
 	}
 
