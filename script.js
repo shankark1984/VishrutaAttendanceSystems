300159aba0b888bf8eb1bc0b678177f2fed7fd36
shankark1984 <46882965+shankark1984@users.noreply.github.com>
2024-07-07 08:36:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // script.js file
-var infoSplitA=null;
-var infoSplitB=null;
-var siteID=null;
-var workOrderNo=null;
-var currentdatetime=null;
+var infoSplitA = null;
+var infoSplitB = null;
+var siteID = null;
+var workOrderNo = null;
+var currentdatetime = null;
+var matchEmpID =null;
 
 function domReady(fn) {
 	if (
@@ -25,22 +26,25 @@ domReady(function () {
 		var mystring = decodeText, decodeResult;
 		var splits = mystring.split(",");
 
-		 if (splits[0] == "Site") {
-			if(infoSplitA==null){
+		if (splits[0] == "Site") {
+			if (infoSplitA == null) {
 				alert("First scan employee card");
 				return;
 			}
 			siteID = splits[1];
 			workOrderNo = splits[2];
 			//alert("Site");
-		} 
-		if(splits[0] == "Emp") {
+		}
+		if (splits[0] == "Emp") {
+			if(splits[1]=!matchEmpID){
+				alert("no Emp list");
+				return;
+			}
 			infoSplitA = splits[1];
 			infoSplitB = splits[2];
 			//alert("Emp");
 		}
-		currentdatetime=new Date().toLocaleString();
-		//alert(splits[0]); // output: this
+		currentdatetime = new Date().toLocaleString();
 
 		//document.getElementById("Info1").textContent = decodeText, decodeResult;
 		document.getElementById("empID").textContent = infoSplitA;
@@ -58,3 +62,13 @@ domReady(function () {
 	htmlscanner.render(onScanSuccess);
 });
 
+const url="https://script.google.com/macros/s/AKfycbwi_6l12-lguoqpNpHdBnHdNKQ5FRqKTFQQcZB8MX6LtG0kDDbquuzxM18zFNQw5Pyk/exec"
+document.getElementById('form').action=url;
+
+fetch('${url}?header=EmployeeCode')
+.then((response)=>response.json())
+.then(({data})=>{
+	document.getElementById(matchEmpID).innerHTML = console.log(data);
+	//document.getElementById(id).innerHTML = matchEmpID;
+	//const matchEmpID=data;
+})
